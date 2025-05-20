acefb0be1159a2487d89e3da89caeed93aafa812
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-05-20 23:03:16 +0530

    fix: code fixed.

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
